8217aff2d4c73e00fbfefec0b5a8802867905c72
kiyusama <kiyusama328@gmail.com>
2026-02-26 20:21:37 +0000

    feat: add genId func

--- frontend/src/views/Lobby.vue
+++ frontend/src/views/Lobby.vue
@@ -1,6 +1,16 @@
 <script setup>
 import { useRouter } from 'vue-router'
 const router = useRouter()
+
+//roomIdを大小英数字で生成
+const generateId = () => {
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  let result = ''
+  for (let i = 0; i < length; i++) {
+    result += chars.charAt(Math.floor(Math.random() * chars.length))
+  }
+  return result
+}
 </script>
 
 <template>
